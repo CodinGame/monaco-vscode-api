91a6e79da18be23f331a3970ec6275de28eb9331
Loïc Mangeonjean <loic@codingame.com>
2022-06-13 11:53:48 +0200

    fix: Prevent missing fields by generating code

--- rollup/rollup.config.ts
+++ rollup/rollup.config.ts
@@ -5,6 +5,7 @@ import * as babel from '@babel/core'
 import * as monaco from 'monaco-editor'
 import typescript from '@rollup/plugin-typescript'
 import cleanup from 'js-cleanup'
+import ts from 'typescript'
 import * as fs from 'fs'
 import * as path from 'path'
 import * as vm from 'vm'
@@ -90,10 +91,55 @@ export default rollup.defineConfig({
       }
     },
     typescript({
-      noEmitOnError: true
+      noEmitOnError: true,
+      transformers: {
+        before: [{
+          type: 'program',
+          factory: function factory (program) {
+            return function transformerFactory (context) {
+              return function transformer (sourceFile) {
+                if (sourceFile.fileName.endsWith('api.ts')) {
+                  let exportEqualsFound = false
+                  function visitor (node: ts.Node): ts.Node {
+                    // Transform `export = api` to `export { field1, field2, ... } = api` as the first syntax is not supported when generating ESM
+                    if (ts.isExportAssignment(node) && (node.isExportEquals ?? false)) {
+                      if (ts.isIdentifier(node.expression)) {
+                        const declaration = program.getTypeChecker().getSymbolAtLocation(node.expression)!.declarations![0]!
+                        if (ts.isVariableDeclaration(declaration) && declaration.initializer != null && ts.isObjectLiteralExpression(declaration.initializer)) {
+                          const propertyNames = declaration.initializer.properties.map(prop => (prop.name as ts.Identifier).text)
+                          exportEqualsFound = true
+                          return context.factory.createVariableStatement([
+                            context.factory.createModifier(ts.SyntaxKind.ExportKeyword)
+                          ], context.factory.createVariableDeclarationList([
+                            context.factory.createVariableDeclaration(
+                              context.factory.createObjectBindingPattern(
+                                propertyNames.map(name => context.factory.createBindingElement(undefined, undefined, context.factory.createIdentifier(name)))
+                              ),
+                              undefined,
+                              undefined,
+                              node.expression
+                            )
+                          ], ts.NodeFlags.Const))
+                        }
+                      }
+                    }
+                    return node
+                  }
+                  const transformed = ts.visitEachChild(sourceFile, visitor, context)
+                  if (!exportEqualsFound) {
+                    throw new Error('`export =` not found in api.ts')
+                  }
+                  return transformed
+                }
+                return sourceFile
+              }
+            }
+          }
+        }]
+      }
     }),
     {
-      name: 'transformations',
+      name: 'improve-vscode-treeshaking',
       transform (code, id) {
         if (id.startsWith(VSCODE_DIR)) {
           const ast = recast.parse(code, {
