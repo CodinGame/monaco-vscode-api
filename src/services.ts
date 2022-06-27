5cc768dce1f51136ea18b8832f2fa16e75c02d3c
Loïc Mangeonjean <loic@codingame.com>
2022-06-27 18:47:18 +0200

    feat: Export monaco service interfaces

--- src/services.ts
+++ src/services.ts
@@ -52,3 +52,36 @@ export namespace Services {
     }
   }
 }
+
+// Export all services as monaco doesn't export them
+export { StandaloneServices } from 'vs/editor/standalone/browser/standaloneServices'
+
+export { ICommandService } from 'vs/platform/commands/common/commands'
+export { INotificationService } from 'vs/platform/notification/common/notification'
+export { IBulkEditService } from 'vs/editor/browser/services/bulkEditService'
+export { ICodeEditorService } from 'vs/editor/browser/services/codeEditorService'
+export { ILanguageService } from 'vs/editor/common/languages/language'
+export { ILanguageConfigurationService } from 'vs/editor/common/languages/languageConfigurationRegistry'
+export { IEditorWorkerService } from 'vs/editor/common/services/editorWorker'
+export { ILanguageFeaturesService } from 'vs/editor/common/services/languageFeatures'
+export { IModelService } from 'vs/editor/common/services/model'
+export { ITextModelService } from 'vs/editor/common/services/resolverService'
+export { IClipboardService } from 'vs/platform/clipboard/common/clipboardService'
+export { IDialogService } from 'vs/platform/dialogs/common/dialogs'
+export { IFileService } from 'vs/platform/files/common/files'
+export { IInstantiationService } from 'vs/platform/instantiation/common/instantiation'
+export { IMarkerService } from 'vs/platform/markers/common/markers'
+export { IOpenerService } from 'vs/platform/opener/common/opener'
+export { IProductService } from 'vs/platform/product/common/productService'
+export { IQuickInputService } from 'vs/platform/quickinput/common/quickInput'
+export { ITelemetryService } from 'vs/platform/telemetry/common/telemetry'
+export { IUriIdentityService } from 'vs/platform/uriIdentity/common/uriIdentity'
+export { IEditorGroupsService } from 'vs/workbench/services/editor/common/editorGroupsService'
+export { IEditorService } from 'vs/workbench/services/editor/common/editorService'
+export { IWorkbenchEnvironmentService } from 'vs/workbench/services/environment/common/environmentService'
+export { IHostService } from 'vs/workbench/services/host/browser/host'
+export { ILanguageStatusService } from 'vs/workbench/services/languageStatus/common/languageStatusService'
+export { IPaneCompositePartService } from 'vs/workbench/services/panecomposite/browser/panecomposite'
+export { IPathService } from 'vs/workbench/services/path/common/pathService'
+export { ITextFileService } from 'vs/workbench/services/textfile/common/textfiles'
+export { IWorkingCopyFileService } from 'vs/workbench/services/workingCopy/common/workingCopyFileService'
