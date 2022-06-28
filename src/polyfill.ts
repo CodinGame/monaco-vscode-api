5503fe56603bd65e7af6d2dc3e4efbf7e57a38a2
Loïc Mangeonjean <loic@codingame.com>
2022-06-28 12:59:41 +0200

    fix: Polyfill quickpick

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,7 +1,26 @@
 
+import { QuickInputController } from 'vs/base/parts/quickinput/browser/quickInput'
 // @ts-ignore
 import { VSBuffer as MonacoVSBuffer } from 'monaco-editor/esm/vs/base/common/buffer.js'
 import { VSBuffer as VScodeVSBuffer } from 'vscode/vs/base/common/buffer.js'
+
+const createQuickPick = QuickInputController.prototype.createQuickPick
+QuickInputController.prototype.createQuickPick = function () {
+  const quickPick = createQuickPick.call(this)
+  // @ts-ignore
+  quickPick.onDidTriggerButton = quickPick.onDidTriggerButtonEmitter.event
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return quickPick as any
+}
+
+const createInputBox = QuickInputController.prototype.createQuickPick
+QuickInputController.prototype.createInputBox = function () {
+  const inputBox = createInputBox.call(this)
+  // @ts-ignore
+  inputBox.onDidTriggerButton = inputBox.onDidTriggerButtonEmitter.event
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return inputBox as any
+}
 
 // A lot of methods from VSBuffer are treeshaked out of monaco editor, we need to restore them here
 // Also we cannot just use the VSCode impl because:
