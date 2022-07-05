c6d61c6cd4abb736563169c6f7a1c1800dc1c09d
Loïc Mangeonjean <loic@codingame.com>
2022-07-05 18:36:14 +0200

    fix: Add more polyfill

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -3,7 +3,21 @@ import { QuickInputController } from 'vs/base/parts/quickinput/browser/quickInpu
 // @ts-ignore Creating a d.ts is not worth it
 import { VSBuffer as MonacoVSBuffer } from 'monaco-editor/esm/vs/base/common/buffer.js'
 import { VSBuffer as VScodeVSBuffer } from 'vscode/vs/base/common/buffer.js'
+// @ts-ignore Creating a d.ts is not worth it
+import { Themable as MonacoThemable } from 'monaco-editor/esm/vs/platform/theme/common/themeService.js'
+import { Themable as VScodeThemable } from 'vscode/vs/platform/theme/common/themeService.js'
+// @ts-ignore Creating a d.ts is not worth it
+import { ProgressBar as MonacoProgressBar } from 'monaco-editor/esm/vs/base/browser/ui/progressbar/progressbar.js'
+import { ProgressBar as VScodeProgressBar } from 'vscode/vs/base/browser/ui/progressbar/progressbar.js'
+import { getSingletonServiceDescriptors } from 'vs/platform/instantiation/common/extensions'
+import { ILabelService } from 'vs/platform/label/common/label'
+import { Event } from 'vs/base/common/event'
 import { IQuickInput, IQuickPick } from 'vs/base/parts/quickinput/common/quickInput'
+
+(MonacoProgressBar as typeof VScodeProgressBar).prototype.hide = VScodeProgressBar.prototype.hide
+
+// eslint-disable-next-line dot-notation
+;(MonacoThemable as typeof VScodeThemable).prototype['getColor'] ??= VScodeThemable.prototype['getColor']
 
 function polyfillQuickInput<T extends IQuickInput> (fn: () => T): () => T {
   return function (this: QuickInputController) {
@@ -18,9 +32,12 @@ function polyfillQuickInput<T extends IQuickInput> (fn: () => T): () => T {
 QuickInputController.prototype.createQuickPick = polyfillQuickInput<IQuickPick<any>>(QuickInputController.prototype.createQuickPick)
 QuickInputController.prototype.createInputBox = polyfillQuickInput(QuickInputController.prototype.createInputBox)
 
+const StandaloneUriLabelService = getSingletonServiceDescriptors().find(([id]) => id === ILabelService)![1].ctor
+StandaloneUriLabelService.prototype.onDidChangeFormatters ??= Event.None
+
 // A lot of methods from VSBuffer are treeshaked out of monaco editor, we need to restore them here
 // Also we cannot just use the VSCode impl because:
-// - The vscode code received instances created inside monaco code
+// - The vscode code receives instances created inside monaco code
 // - VSCode does `instanceof` on instances that need to work
 
 function toVSCodeVSBuffer (value: unknown) {
