a31f8790a8bbd033e0f80e171117a4630a1b8b70
Loïc Mangeonjean <loic@codingame.com>
2022-07-07 15:16:28 +0200

    fix: Typo

--- src/override/vs/workbench/services/textfile/common/textFileEditorModel.js
+++ src/override/vs/workbench/services/textfile/common/textFileEditorModel.js
@@ -1,5 +1,5 @@
 export * from 'vs/workbench/services/textfile/common/textFileEditorModel'
 
-// Replace right BaseTextEditor implementation by an empty class
+// Replace right TextFileEditorModel implementation by an empty class
 // This class is only used in an `instanceof` always false but pulls a lot of code
 export class TextFileEditorModel {}
