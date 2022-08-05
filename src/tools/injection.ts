92f54d7d4d0b5260987c18e8d0be038d402fc859
Loïc Mangeonjean <loic@codingame.com>
2022-08-05 14:23:19 +0200

    fix: Improve injection tools

--- src/tools/injection.ts
+++ src/tools/injection.ts
@@ -1,6 +1,6 @@
 import { BrandedService, IInstantiationService, _util } from 'vs/platform/instantiation/common/instantiation'
 
-function getInjectedParameters<Ctor extends new (...args: any[]) => InstanceType<Ctor>> (instantiationService: IInstantiationService, ctor: Ctor) {
+function getInjectedParameters<Ctor extends abstract new (...args: any[]) => InstanceType<Ctor>> (instantiationService: IInstantiationService, ctor: Ctor) {
   return instantiationService.invokeFunction((accessor) => {
     return _util.getServiceDependencies(ctor).sort((a, b) => a.index - b.index).map(d => accessor.get(d.id))
   })
@@ -13,9 +13,9 @@ declare type GetLeadingNonServiceArgs<Args> = Args extends [...BrandedService[]]
  * @param ctor The class to inject
  * @returns A class that only needs the injector
  */
-export function createInjectedClass<Ctor extends new (...args: any[]) => InstanceType<Ctor>>(ctor: Ctor): new (instantiationService: IInstantiationService, ...args: GetLeadingNonServiceArgs<ConstructorParameters<Ctor>>) => InstanceType<Ctor> {
+export function createInjectedClass<Ctor extends abstract new (...args: any[]) => InstanceType<Ctor>>(ctor: Ctor): new (instantiationService: IInstantiationService, ...args: GetLeadingNonServiceArgs<ConstructorParameters<Ctor>>) => InstanceType<Ctor> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const _ctor = (ctor as (new (...args: any[]) => any))
+  const _ctor = (ctor as (abstract new (...args: any[]) => any))
   return class extends _ctor {
     constructor (...args: any[]) {
       super(...args.slice(1), ...getInjectedParameters(args[0], ctor))
