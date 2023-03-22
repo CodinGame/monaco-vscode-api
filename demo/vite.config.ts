83b87877d4ee1f11d5cf8b829d0411c4b21491e8
Loïc Mangeonjean <loic@coderpad.io>
2023-03-22 13:14:44 +0100

    fix(demo): force port

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     origin: 'http://localhost:5173',
+    port: 5173,
     fs: {
       allow: ['../'] // allow to load codicon.ttf from monaco-editor in the parent folder
     }
