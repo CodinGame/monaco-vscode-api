d69480fb71078b8005599c1e566e8f350da69026
Loïc Mangeonjean <loic@coderpad.io>
2023-04-07 16:07:02 +0200

    feat: use vscode WorkspaceService

--- src/tools.ts
+++ src/tools.ts
@@ -1,5 +1,24 @@
+
 export function unsupported (): never {
   throw new Error('unsupported')
 }
 
 export const noop = (): void => {}
+
+export function memoized<A extends unknown[], T> (fct: (...args: A) => T): (...args: A) => T {
+  let v: T | null = null
+  return (...args) => {
+    if (v == null) {
+      v = fct(...args)
+    }
+    return v
+  }
+}
+
+export function memoizedConstructor<T> (ctor: new (...args: any[]) => T): new (...args: any[]) => T {
+  return new Proxy(ctor, {
+    construct: memoized((target, args) => {
+      return Reflect.construct(ctor, args) as object
+    })
+  })
+}
