060288c17872780eab517d162529c9064fa7f490
Loïc Mangeonjean <loic@coderpad.io>
2023-05-31 12:08:34 +0200

    fix: load services only when ready

--- src/service-override/textmate.ts
+++ src/service-override/textmate.ts
@@ -1,12 +1,12 @@
 import '../vscode-services/missing-services'
-import { IEditorOverrideServices } from 'vs/editor/standalone/browser/standaloneServices'
+import { IEditorOverrideServices, StandaloneServices } from 'vs/editor/standalone/browser/standaloneServices'
 import { ITextMateTokenizationService } from 'vs/workbench/services/textMate/browser/textMateTokenizationFeature'
 import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors'
 import { TextMateTokenizationFeature } from 'vs/workbench/services/textMate/browser/textMateTokenizationFeatureImpl'
 import { TokenClassificationExtensionPoints } from 'vs/workbench/services/themes/common/tokenClassificationExtensionPoint'
 import { IWorkbenchContribution, IWorkbenchContributionsRegistry, Extensions as WorkbenchExtensions } from 'vs/workbench/common/contributions'
 import { Registry } from 'vs/platform/registry/common/platform'
-import { LifecyclePhase } from 'vs/workbench/services/lifecycle/common/lifecycle'
+import { ILifecycleService, LifecyclePhase } from 'vs/workbench/services/lifecycle/common/lifecycle'
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation'
 import _onigWasm from 'vscode-oniguruma/release/onig.wasm'
 import getFileServiceOverride from './files'
@@ -17,8 +17,10 @@ registerAssets({
 })
 
 registerServiceInitializeParticipant(async (accessor) => {
-  // Force load the service
-  accessor.get(ITextMateTokenizationService)
+  void accessor.get(ILifecycleService).when(LifecyclePhase.Ready).then(() => {
+    // Force load the service
+    StandaloneServices.get(ITextMateTokenizationService)
+  })
 })
 
 class ExtensionPoints implements IWorkbenchContribution {
