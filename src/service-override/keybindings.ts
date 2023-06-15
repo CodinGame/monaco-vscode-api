b521f69df9f070026d138090257938d481f69c18
Loïc Mangeonjean <loic@coderpad.io>
2023-06-15 18:18:00 +0200

    feat: use vscode command service

--- src/service-override/keybindings.ts
+++ src/service-override/keybindings.ts
@@ -8,6 +8,8 @@ import { IUserDataProfilesService } from 'vs/platform/userDataProfile/common/use
 import { IKeyboardLayoutService } from 'vs/platform/keyboardLayout/common/keyboardLayout'
 import { BrowserKeyboardLayoutService } from 'vs/workbench/services/keybinding/browser/keyboardLayoutService'
 import { IFileService } from 'vs/platform/files/common/files'
+import { ICommandService } from 'vs/platform/commands/common/commands'
+import { CommandService } from 'vs/workbench/services/commands/common/commandService'
 import getFileServiceOverride from './files'
 import 'vs/workbench/browser/workbench.contribution'
 
@@ -20,7 +22,8 @@ export default function getServiceOverride (): IEditorOverrideServices {
   return {
     ...getFileServiceOverride(),
     [IKeybindingService.toString()]: new SyncDescriptor(WorkbenchKeybindingService),
-    [IKeyboardLayoutService.toString()]: new SyncDescriptor(BrowserKeyboardLayoutService, undefined, true)
+    [IKeyboardLayoutService.toString()]: new SyncDescriptor(BrowserKeyboardLayoutService, undefined, true),
+    [ICommandService.toString()]: new SyncDescriptor(CommandService)
   }
 }
 
