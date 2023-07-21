befe617c15ba7dd3475d46a029715de38b879da0
Loïc Mangeonjean <loic@coderpad.io>
2023-07-21 11:40:23 +0200

    feat: mock semver

---
+++ src/override/vs/base/common/semver/semver.ts
@@ -0,0 +1,5 @@
+export const gte = (): boolean => false
+export const gt = (): boolean => false
+export const lt = (): boolean => false
+export const lte = (): boolean => false
+export const valid = (): boolean => false
