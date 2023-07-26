191dffa62390fabf719412775ade050259251e47
Loïc Mangeonjean <loic@coderpad.io>
2023-07-26 21:27:24 +0200

    clean: remove useless types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,12 +1,2 @@
 declare const VSCODE_VERSION: string
 declare const VSCODE_REF: string
-
-declare module '*.wasm' {
-  const url: string
-  export default url
-}
-
-declare module '*.html' {
-  const content: string
-  export default content
-}
