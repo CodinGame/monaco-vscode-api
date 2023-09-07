ff0bd3d5610509c989988bd79ad246fe7afd5518
Ethan Parizeau <ethanparizeau@gmail.com>
2023-09-07 11:59:58 -0500

    Changed cdnDomain in demo/vite.config.ts to point to https://127.0.0.1:5173 to resolve CORS error

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import * as fs from 'fs'
 import url from 'url'
 
-const cdnDomain = 'http://127.0.0.2:5173'
+//const cdnDomain = 'http://127.0.0.2:5173'
+const cdnDomain = 'http://127.0.0.1:5173' // This fixes the issue with CORS
+
 
 export default defineConfig({
   build: {
