c106bb51cf8fec8faf97514a778372cd0c8a346d
Loïc Mangeonjean <loic@coderpad.io>
2023-09-07 18:02:48 +0200

    feat: lifecycle service override

---
+++ src/service-override/lifecycle.ts
@@ -0,0 +1,11 @@
+import '../missing-services'
+import { IEditorOverrideServices } from 'vs/editor/standalone/browser/standaloneServices'
+import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors'
+import { BrowserLifecycleService } from 'vs/workbench/services/lifecycle/browser/lifecycleService'
+import { ILifecycleService } from 'vs/workbench/services/lifecycle/common/lifecycle'
+
+export default function getServiceOverride (): IEditorOverrideServices {
+  return {
+    [ILifecycleService.toString()]: new SyncDescriptor(BrowserLifecycleService)
+  }
+}
