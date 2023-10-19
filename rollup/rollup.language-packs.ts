36797b5e5e308c159c3bd36c8ad2b114e4d8435a
Loïc Mangeonjean <loic@coderpad.io>
2023-10-19 17:55:24 +0200

    feat: build language packs

---
+++ rollup/rollup.language-packs.ts
@@ -0,0 +1,127 @@
+import nodeResolve from '@rollup/plugin-node-resolve'
+import * as rollup from 'rollup'
+import { IExtensionManifest } from 'vs/platform/extensions/common/extensions'
+import { dataToEsm } from '@rollup/pluginutils'
+import { PackageJson } from 'type-fest'
+import * as fs from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+import * as fsPromise from 'fs/promises'
+import pkg from '../package.json' assert { type: 'json' }
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const EXTENSIONS = ['', '.ts', '.js']
+
+const BASE_DIR = path.resolve(__dirname, '..')
+const LOC_PATH = path.resolve(BASE_DIR, 'vscode-loc')
+
+const locExtensions = fs.readdirSync(LOC_PATH, { withFileTypes: true })
+  .filter(f => f.isDirectory() && fs.existsSync(path.resolve(LOC_PATH, f.name, 'package.json')))
+  .map(f => f.name)
+
+export default rollup.defineConfig([
+  ...locExtensions.map(name => (<rollup.RollupOptions>{
+    input: {
+      index: path.resolve(LOC_PATH, name)
+    },
+    output: [{
+      minifyInternalExports: false,
+      preserveModules: true,
+      assetFileNames: '[name][extname]',
+      format: 'esm',
+      dir: `dist/${name}`,
+      entryFileNames: '[name].js',
+      hoistTransitiveImports: false
+    }],
+    external (source) {
+      return source === 'vscode/l10n'
+    },
+    plugins: [
+      {
+        name: 'resolve-asset-url',
+        resolveFileUrl (options) {
+          let relativePath = options.relativePath
+          if (!relativePath.startsWith('.')) {
+            relativePath = `./${options.relativePath}`
+          }
+          return `'${relativePath}'`
+        }
+      },
+      {
+        name: 'loader',
+        resolveId (source) {
+          return source
+        },
+        async load (id) {
+          if (path.dirname(id) === LOC_PATH) {
+            const packageJson: IExtensionManifest = JSON.parse((await fsPromise.readFile(path.resolve(id, 'package.json'))).toString())
+            const mainLocalization = packageJson.contributes!.localizations![0]!
+
+            const mainTranslation = mainLocalization.translations.find(t => t.id === 'vscode')!
+            const otherTranslations = mainLocalization.translations.filter(t => t.id !== 'vscode')!
+
+            const translationAssets: Record<string, string> = Object.fromEntries(await Promise.all(otherTranslations.map(async t => {
+              const assetRef = this.emitFile({
+                type: 'asset',
+                name: path.relative('.', t.path),
+                source: await fsPromise.readFile(path.resolve(id, t.path))
+              })
+              return [t.id, assetRef]
+            })))
+
+            return `
+import { registerLocalization } from 'vscode/l10n'
+import content from '${path.resolve(id, mainTranslation.path)}'
+registerLocalization('${mainLocalization.languageId}', content, {
+${Object.entries(translationAssets).map(([id, assetRef]) => `  '${id}': new URL(import.meta.ROLLUP_FILE_URL_${assetRef}, import.meta.url).href`).join(',\n')}
+})
+  `
+          }
+          return undefined
+        },
+        transform (code, id) {
+          if (!id.endsWith('.json')) return null
+
+          const parsed = JSON.parse(code)
+          return {
+            code: dataToEsm(parsed.contents, {
+              preferConst: true
+            }),
+            map: { mappings: '' }
+          }
+        },
+        generateBundle () {
+          const packageJson: PackageJson = {
+            name: `@codingame/monaco-${name.toLowerCase()}`,
+            ...Object.fromEntries(Object.entries(pkg).filter(([key]) => ['version', 'keywords', 'author', 'license', 'repository', 'type'].includes(key))),
+            private: false,
+            description: `Language pack designed to be used with ${pkg.name}`,
+            main: 'index.js',
+            module: 'index.js',
+            types: 'index.d.ts',
+            dependencies: {
+              vscode: `npm:${pkg.name}@^${pkg.version}`
+            }
+          }
+          this.emitFile({
+            fileName: 'package.json',
+            needsCodeReference: false,
+            source: JSON.stringify(packageJson, null, 2),
+            type: 'asset'
+          })
+
+          this.emitFile({
+            fileName: 'index.d.ts',
+            needsCodeReference: false,
+            source: 'export {}',
+            type: 'asset'
+          })
+        }
+      },
+      nodeResolve({
+        extensions: EXTENSIONS
+      })
+    ]
+  }))
+])
