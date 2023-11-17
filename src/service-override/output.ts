f55a34197e8e06311674bdbfb476a678ed5183a9
Loïc Mangeonjean <loic@coderpad.io>
2023-11-17 12:34:20 +0100

    fix: make output service use log service override

--- src/service-override/output.ts
+++ src/service-override/output.ts
@@ -1,17 +1,10 @@
 import { IEditorOverrideServices } from 'vs/editor/standalone/browser/standaloneServices'
+import { LogLevel } from 'vs/platform/log/common/log'
 import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors'
-import { IFileService } from 'vs/platform/files/common/files'
-import { ILoggerService, LogLevel, getLogLevel } from 'vs/platform/log/common/log'
-import { FileLoggerService } from 'vs/platform/log/common/fileLog'
+import { OutputService } from 'vs/workbench/contrib/output/browser/outputServices'
+import { IOutputService } from 'vs/workbench/services/output/common/output'
+import getLogServiceOverride from './log'
 import 'vs/workbench/contrib/output/browser/output.contribution'
-import { IEnvironmentService } from 'vs/platform/environment/common/environment'
-import { logsPath } from '../workbench'
-
-class _FileLoggerService extends FileLoggerService {
-  constructor (logLevel: LogLevel | undefined, @IFileService fileService: IFileService, @IEnvironmentService environmentService: IEnvironmentService) {
-    super(logLevel ?? getLogLevel(environmentService), logsPath, fileService)
-  }
-}
 
 function getServiceOverride (): IEditorOverrideServices
 /**
@@ -21,7 +14,8 @@ function getServiceOverride (logLevel?: LogLevel): IEditorOverrideServices
 
 function getServiceOverride (logLevel?: LogLevel): IEditorOverrideServices {
   return {
-    [ILoggerService.toString()]: new SyncDescriptor(_FileLoggerService, [logLevel], true)
+    ...getLogServiceOverride(logLevel),
+    [IOutputService.toString()]: new SyncDescriptor(OutputService, undefined, true)
   }
 }
 
