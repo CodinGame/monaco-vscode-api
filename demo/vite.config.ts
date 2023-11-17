b5d29cc9a9cd1dfcd583c403ff5cbb532bb9cf5e
Ives van Hoorne <ives@codesandbox.io>
2023-11-17 15:19:02 +0100

    fix: change corp settings to support loading images

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       apply: 'serve',
       configureServer: server => {
         server.middlewares.use((_req, res, next) => {
-          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+          res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless')
           res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
           res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin')
           next()
