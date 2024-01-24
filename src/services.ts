651f1871f7c271949b000aa872f51f7176c20171
Loïc Mangeonjean <loic@coderpad.io>
2024-01-24 15:12:16 +0100

    feat!: import editor services and contributions

--- src/services.ts
+++ src/services.ts
@@ -1,5 +1,6 @@
 import './missing-services'
 import './contributions'
+import 'vs/editor/editor.main'
 import Severity from 'vs/base/common/severity'
 import { IConfigurationChangeEvent } from 'vs/platform/configuration/common/configuration'
 import { ITextModelContentProvider } from 'vs/editor/common/services/resolverService'
