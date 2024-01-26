c504ab6867c9b63260dea349e2fad8bbce960057
Loïc Mangeonjean <loic@coderpad.io>
2024-01-26 12:05:24 +0100

    refactor: use published esbuild plugin

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import glob from 'fast-glob'
+import importMetaUrlPlugin from '@codingame/esbuild-import-meta-url-plugin'
 import * as fs from 'fs'
-import url from 'url'
 import path from 'path'
 import pkg from './package.json' assert { type: 'json' }
 
@@ -65,32 +65,7 @@ export default defineConfig({
     exclude: [],
     esbuildOptions: {
       tsconfig: './tsconfig.json',
-      plugins: [{
-        name: 'import.meta.url',
-        setup ({ onLoad }) {
-          // Help vite that bundles/move files in dev mode without touching `import.meta.url` which breaks asset urls
-          onLoad({ filter: /.*\.js/, namespace: 'file' }, async args => {
-            const code = fs.readFileSync(args.path, 'utf8')
-
-            const assetImportMetaUrlRE = /\bnew\s+URL\s*\(\s*('[^']+'|"[^"]+"|`[^`]+`)\s*,\s*import\.meta\.url\s*(?:,\s*)?\)/g
-            let i = 0
-            let newCode = ''
-            for (let match = assetImportMetaUrlRE.exec(code); match != null; match = assetImportMetaUrlRE.exec(code)) {
-              newCode += code.slice(i, match.index)
-
-              const path = match[1].slice(1, -1)
-              const resolved = await import.meta.resolve!(path, url.pathToFileURL(args.path))
-
-              newCode += `new URL(${JSON.stringify(url.fileURLToPath(resolved))}, import.meta.url)`
-
-              i = assetImportMetaUrlRE.lastIndex
-            }
-            newCode += code.slice(i)
-
-            return { contents: newCode }
-          })
-        }
-      }]
+      plugins: [importMetaUrlPlugin]
     }
   },
   server: {
