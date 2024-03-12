1779834e0a067083592a137ff17a8f9d47cf3a85
Loïc Mangeonjean <loic@coderpad.io>
2024-03-12 18:06:04 +0100

    fix: display error when language pack is loaded too late

--- src/l10n.ts
+++ src/l10n.ts
@@ -1,8 +1,11 @@
-import { setLocale } from 'vs/nls'
+import { setLocale, isInitialized } from 'vs/nls'
 
 const extensionTranslationsUri: Record<string, Record<string, string>> = {}
 
 function registerLocalization (language: string, main: Record<string, Record<string, string>>, extensionTranslationsUris: Record<string, string>): void {
+  if (isInitialized()) {
+    console.error('Some parts of VSCode are already initialized, make sure the language pack is loaded before anything else or some translations will be missing')
+  }
   setLocale(language, main)
 
   extensionTranslationsUri[language] = extensionTranslationsUris
