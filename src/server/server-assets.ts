18d370b49ce4c63b837dce27fe3b33703ccf6686
Loïc Mangeonjean <loic@coderpad.io>
2024-03-27 17:53:04 +0100

    cleanup: remove ext host server

--- src/server/server-assets.ts
+++
@@ -1,6 +0,0 @@
-import { registerAssets } from '../assets'
-
-registerAssets({
-  'vs/code/browser/workbench/workbench.html': new URL('vs/code/browser/workbench/workbench.html', import.meta.url).toString(),
-  'bootstrap-fork': new URL('bootstrap-fork.js', import.meta.url).toString()
-})
