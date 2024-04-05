2227097230b2dfbaf4d0a6fcffffb10867a836ef
Ives van Hoorne <ives@codesandbox.io>
2024-04-05 21:30:16 +0200

    feat: add secret storage service
    This service is used by some extensions in VSCode

---
+++ src/service-override/secretStorage.ts
@@ -0,0 +1,15 @@
+import { IEditorOverrideServices } from 'vs/editor/standalone/browser/standaloneServices'
+import { IEncryptionService } from 'vs/platform/encryption/common/encryptionService'
+import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors'
+import { ISecretStorageService } from 'vs/platform/secrets/common/secrets'
+import { EncryptionService } from 'vs/workbench/services/encryption/browser/encryptionService'
+import { BrowserSecretStorageService } from 'vs/workbench/services/secrets/browser/secretStorageService'
+
+export { LocalStorageSecretStorageProvider } from 'vs/code/browser/workbench/workbench'
+
+export default function getServiceOverride (): IEditorOverrideServices {
+  return {
+    [ISecretStorageService.toString()]: new SyncDescriptor(BrowserSecretStorageService, [], true),
+    [IEncryptionService.toString()]: new SyncDescriptor(EncryptionService, [], true)
+  }
+}
