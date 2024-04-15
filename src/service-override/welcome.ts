1d25203a530bdebc55b3fc74c415d13706b1a356
Loïc Mangeonjean <loic@coderpad.io>
2024-04-15 11:18:02 +0200

    feat: Split welcome and walkthrough contributions

--- src/service-override/welcome.ts
+++ src/service-override/welcome.ts
@@ -1,24 +1,8 @@
 import { IEditorOverrideServices } from 'vs/editor/standalone/browser/standaloneServices'
-import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors'
-import { IWalkthroughsService, WalkthroughsService } from 'vs/workbench/contrib/welcomeGettingStarted/browser/gettingStartedService'
-import 'vs/workbench/contrib/welcomeGettingStarted/browser/gettingStarted.contribution'
-import 'vs/workbench/contrib/welcomeWalkthrough/browser/walkThrough.contribution'
 import 'vs/workbench/contrib/welcomeViews/common/viewsWelcome.contribution'
 import 'vs/workbench/contrib/welcomeViews/common/newFile.contribution'
-import svgAssets from 'vs/workbench/contrib/welcomeGettingStarted/common/media/*.svg'
-import pngAssets from 'vs/workbench/contrib/welcomeGettingStarted/common/media/*.png'
-import { FileAccess } from 'vs/base/common/network'
-import { registerAssets } from '../assets'
-
-registerAssets(svgAssets)
-registerAssets(pngAssets)
-
-FileAccess.registerModuleContentProvider('vs/workbench/contrib/welcomeWalkthrough/browser/editor/vs_code_editor_walkthrough', () => import('vs/workbench/contrib/welcomeWalkthrough/browser/editor/vs_code_editor_walkthrough'))
-FileAccess.registerModuleContentProvider('vs/workbench/contrib/welcomeGettingStarted/common/media/theme_picker', () => import('vs/workbench/contrib/welcomeGettingStarted/common/media/theme_picker'))
-FileAccess.registerModuleContentProvider('vs/workbench/contrib/welcomeGettingStarted/common/media/notebookProfile', () => import('vs/workbench/contrib/welcomeGettingStarted/common/media/notebookProfile'))
 
 export default function getServiceOverride (): IEditorOverrideServices {
   return {
-    [IWalkthroughsService.toString()]: new SyncDescriptor(WalkthroughsService, [], true)
   }
 }
