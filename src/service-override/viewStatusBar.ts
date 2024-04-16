b2470cb582e61b91230922ef066ebc35d7b0e858
Loïc Mangeonjean <loic@coderpad.io>
2024-04-16 15:38:03 +0200

    refactor: import splitted extension points

--- src/service-override/viewStatusBar.ts
+++ src/service-override/viewStatusBar.ts
@@ -1,21 +1,8 @@
 import { IEditorOverrideServices } from 'vs/editor/standalone/browser/standaloneServices'
 import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors'
-import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation'
 import { StatusbarService } from 'vs/workbench/browser/parts/statusbar/statusbarPart'
 import { IStatusbarService } from 'vs/workbench/services/statusbar/browser/statusbar'
-import { IWorkbenchContribution, WorkbenchPhase, registerWorkbenchContribution2 } from 'vs/workbench/common/contributions'
-import { ExtensionStatusBarItemService, IExtensionStatusBarItemService, StatusBarItemsExtensionPoint } from 'vs/workbench/api/browser/statusBarExtensionPoint'
-
-class ExtensionPoints implements IWorkbenchContribution {
-  static readonly ID = 'workbench.contrib.extensionPoints.statusBar'
-
-  constructor (
-    @IInstantiationService private readonly instantiationService: IInstantiationService
-  ) {
-    this.instantiationService.createInstance(StatusBarItemsExtensionPoint)
-  }
-}
-registerWorkbenchContribution2(ExtensionPoints.ID, ExtensionPoints, WorkbenchPhase.BlockStartup)
+import { ExtensionStatusBarItemService, IExtensionStatusBarItemService } from 'vs/workbench/api/browser/statusBarExtensionPoint'
 
 export default function getServiceOverride (): IEditorOverrideServices {
   return {
