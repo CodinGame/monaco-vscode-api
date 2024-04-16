6afb354754179007838160203880313e19cd75a1
Loïc Mangeonjean <loic@coderpad.io>
2024-04-16 15:38:54 +0200

    refactor: use splitted extension point and service

--- src/service-override/viewStatusBar.ts
+++ src/service-override/viewStatusBar.ts
@@ -2,7 +2,8 @@ import { IEditorOverrideServices } from 'vs/editor/standalone/browser/standalone
 import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors'
 import { StatusbarService } from 'vs/workbench/browser/parts/statusbar/statusbarPart'
 import { IStatusbarService } from 'vs/workbench/services/statusbar/browser/statusbar'
-import { ExtensionStatusBarItemService, IExtensionStatusBarItemService } from 'vs/workbench/api/browser/statusBarExtensionPoint'
+import { ExtensionStatusBarItemService, IExtensionStatusBarItemService } from 'vs/workbench/api/browser/statusBarService'
+import 'vs/workbench/api/browser/statusBarExtensionPoint'
 
 export default function getServiceOverride (): IEditorOverrideServices {
   return {
