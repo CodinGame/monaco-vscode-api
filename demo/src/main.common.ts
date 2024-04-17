f2b69d42fa597136ad01a4111b91595bf47574c7
Loïc Mangeonjean <loic@coderpad.io>
2024-04-17 17:03:41 +0200

    fix(demo): disable some features when html filesystem is used

--- demo/src/main.common.ts
+++ demo/src/main.common.ts
@@ -52,6 +52,7 @@ import '@codingame/monaco-vscode-npm-default-extension'
 import '@codingame/monaco-vscode-media-preview-default-extension'
 import '@codingame/monaco-vscode-ipynb-default-extension'
 import { ExtensionHostKind, registerExtension } from 'vscode/extensions'
+import { useHtmlFileSystemProvider } from './setup.common'
 
 const { getApi } = registerExtension({
   name: 'demo-main',
@@ -63,20 +64,22 @@ const { getApi } = registerExtension({
 }, ExtensionHostKind.LocalProcess)
 
 void getApi().then(async vscode => {
-  const mainModelUri = vscode.Uri.file('/tmp/test.js')
-  await Promise.all([
-    vscode.workspace.openTextDocument(mainModelUri),
-    vscode.workspace.openTextDocument(monaco.Uri.file('/tmp/test_readonly.js')) // open the file so vscode sees it's locked
-  ])
+  if (!useHtmlFileSystemProvider) {
+    const mainModelUri = vscode.Uri.file('/tmp/test.js')
+    await Promise.all([
+      vscode.workspace.openTextDocument(mainModelUri),
+      vscode.workspace.openTextDocument(monaco.Uri.file('/tmp/test_readonly.js')) // open the file so vscode sees it's locked
+    ])
 
-  const diagnostics = vscode.languages.createDiagnosticCollection('demo')
-  diagnostics.set(mainModelUri, [{
-    range: new vscode.Range(2, 9, 2, 12),
-    severity: vscode.DiagnosticSeverity.Error,
-    message: 'This is not a real error, just a demo, don\'t worry',
-    source: 'Demo',
-    code: 42
-  }])
+    const diagnostics = vscode.languages.createDiagnosticCollection('demo')
+    diagnostics.set(mainModelUri, [{
+      range: new vscode.Range(2, 9, 2, 12),
+      severity: vscode.DiagnosticSeverity.Error,
+      message: 'This is not a real error, just a demo, don\'t worry',
+      source: 'Demo',
+      code: 42
+    }])
+  }
 
   document.querySelector('#toggleFullWorkbench')!.addEventListener('click', async () => {
     const url = new URL(window.location.href)
