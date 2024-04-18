c57aa747652fe037f249ac6d0e130148db9ce7e6
Loïc Mangeonjean <loic@coderpad.io>
2024-04-18 16:37:59 +0200

    cleanup: stop overriding by empty object
    as it seems they are not used anymore from outside and will be only contained by the sync service override

--- src/override/vs/platform/userDataSync/common/extensionsSync.js
+++
@@ -1,5 +0,0 @@
-export * from 'vscode/src/vs/platform/userDataSync/common/extensionsSync'
-
-export class ExtensionsSynchroniser {}
-
-export class AbstractExtensionsInitializer {}
--- src/override/vs/platform/userDataSync/common/globalStateSync.js
+++
@@ -1,7 +0,0 @@
-export * from 'vscode/src/vs/platform/userDataSync/common/globalStateSync'
-
-export class GlobalStateSynchroniser {}
-
-export class UserDataSyncStoreTypeSynchronizer {}
-
-export class GlobalStateInitializer {}
