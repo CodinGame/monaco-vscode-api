743cd196b08d1dfb54b0ccac497a836e03a4de46
Loïc Mangeonjean <loic@coderpad.io>
2024-04-19 18:30:12 +0200

    refactor: create a view-common service override
    to prevent putting view-specific code in the main package

---
+++ src/service-override/viewCommon.ts
@@ -0,0 +1,104 @@
+import { IEditorOverrideServices } from 'vs/editor/standalone/browser/standaloneServices'
+import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors'
+import { IViewDescriptorService } from 'vs/workbench/common/views'
+import { ViewDescriptorService } from 'vs/workbench/services/views/browser/viewDescriptorService'
+import { IActivityService } from 'vs/workbench/services/activity/common/activity'
+import { ActivityService } from 'vs/workbench/services/activity/browser/activityService'
+import { IPaneCompositePartService } from 'vs/workbench/services/panecomposite/browser/panecomposite'
+import { IEditorResolverService } from 'vs/workbench/services/editor/common/editorResolverService'
+import { EditorResolverService } from 'vs/workbench/services/editor/browser/editorResolverService'
+import { BreadcrumbsService, IBreadcrumbsService } from 'vs/workbench/browser/parts/editor/breadcrumbs'
+import { IContextViewService } from 'vs/platform/contextview/browser/contextView'
+import { ContextViewService } from 'vs/platform/contextview/browser/contextViewService'
+import { ICodeEditorService } from 'vs/editor/browser/services/codeEditorService'
+import { ITextEditorService, TextEditorService } from 'vs/workbench/services/textfile/common/textEditorService'
+import { CodeEditorService } from 'vs/workbench/services/editor/browser/codeEditorService'
+import { IUntitledTextEditorService, UntitledTextEditorService } from 'vs/workbench/services/untitled/common/untitledTextEditorService'
+import { IHistoryService } from 'vs/workbench/services/history/common/history'
+import { HistoryService } from 'vs/workbench/services/history/browser/historyService'
+import { ICustomEditorService } from 'vs/workbench/contrib/customEditor/common/customEditor'
+import { CustomEditorService } from 'vs/workbench/contrib/customEditor/browser/customEditors'
+import { WebviewService } from 'vs/workbench/contrib/webview/browser/webviewService'
+import { IWebviewWorkbenchService, WebviewEditorService } from 'vs/workbench/contrib/webviewPanel/browser/webviewWorkbenchService'
+import { IWebviewService } from 'vs/workbench/contrib/webview/browser/webview'
+import { IWebviewViewService, WebviewViewService } from 'vs/workbench/contrib/webviewView/browser/webviewViewService'
+import { IProgressService } from 'vs/platform/progress/common/progress'
+import { ProgressService } from 'vs/workbench/services/progress/browser/progressService'
+import { PaneCompositePartService } from 'vs/workbench/browser/parts/paneCompositePartService'
+import { BrowserAuxiliaryWindowService, IAuxiliaryWindowService } from 'vs/workbench/services/auxiliaryWindow/browser/auxiliaryWindowService'
+import { IViewsService } from 'vs/workbench/services/views/common/viewsService'
+import { ViewsService } from 'vs/workbench/services/views/browser/viewsService'
+import { IHoverService } from 'vs/platform/hover/browser/hover'
+import { IEditorPaneService } from 'vs/workbench/services/editor/common/editorPaneService'
+import { EditorPaneService } from 'vs/workbench/services/editor/browser/editorPaneService'
+import { HoverService } from 'vs/editor/browser/services/hoverService/hoverService'
+import { CustomEditorLabelService, ICustomEditorLabelService } from 'vs/workbench/services/editor/common/customEditorLabelService'
+import getBulkEditServiceOverride from './bulkEdit'
+import { changeUrlDomain } from './tools/url'
+import { registerAssets } from '../assets'
+import 'vs/workbench/contrib/callHierarchy/browser/callHierarchy.contribution'
+import 'vs/workbench/contrib/typeHierarchy/browser/typeHierarchy.contribution'
+import 'vs/workbench/browser/actions/listCommands'
+import 'vscode/src/vs/workbench/browser/parts/views/media/views.css'
+import 'vs/workbench/api/browser/viewsExtensionPoint'
+import 'vs/workbench/browser/parts/editor/editor.contribution'
+import 'vs/workbench/browser/workbench.contribution'
+import 'vs/workbench/contrib/customEditor/browser/customEditor.contribution'
+import 'vs/workbench/contrib/webviewPanel/browser/webviewPanel.contribution'
+import 'vs/workbench/contrib/externalUriOpener/common/externalUriOpener.contribution'
+import 'vs/workbench/contrib/languageStatus/browser/languageStatus.contribution'
+import 'vs/workbench/contrib/mergeEditor/browser/mergeEditor.contribution'
+import 'vs/workbench/contrib/webview/browser/webview.contribution'
+import 'vs/workbench/contrib/limitIndicator/browser/limitIndicator.contribution'
+import 'vs/workbench/contrib/sash/browser/sash.contribution'
+import 'vs/workbench/contrib/preferences/browser/keyboardLayoutPicker'
+import 'vs/workbench/browser/parts/editor/editor.autosave.contribution'
+import 'vs/workbench/contrib/files/browser/files.editorPane.contribution'
+import 'vs/workbench/contrib/files/browser/fileCommands.save'
+import 'vs/workbench/browser/actions/navigationActions'
+import 'vs/workbench/browser/style'
+import './tools/editorAssets'
+
+// Import it from here to force the bundler to put it in this service-override package
+import 'vs/workbench/browser/parts/editor/editorParts'
+
+let webviewIframeAlternateDomains: string | undefined
+registerAssets({
+  'vs/workbench/contrib/webview/browser/pre/service-worker.js': () => changeUrlDomain(new URL('../../vscode/src/vs/workbench/contrib/webview/browser/pre/service-worker.js', import.meta.url).href, webviewIframeAlternateDomains),
+  'vs/workbench/contrib/webview/browser/pre/index.html': () => changeUrlDomain(new URL('../assets/webview/index.html', import.meta.url).href, webviewIframeAlternateDomains),
+  'vs/workbench/contrib/webview/browser/pre/index-no-csp.html': () => changeUrlDomain(new URL('../assets/webview/index-no-csp.html', import.meta.url).href, webviewIframeAlternateDomains),
+  'vs/workbench/contrib/webview/browser/pre/fake.html': () => changeUrlDomain(new URL('../../vscode/src/vs/workbench/contrib/webview/browser/pre/fake.html', import.meta.url).href, webviewIframeAlternateDomains)
+})
+
+function getServiceOverride (_webviewIframeAlternateDomains?: string): IEditorOverrideServices {
+  if (_webviewIframeAlternateDomains != null) {
+    webviewIframeAlternateDomains = _webviewIframeAlternateDomains
+  }
+
+  return {
+    ...getBulkEditServiceOverride(),
+    [IViewsService.toString()]: new SyncDescriptor(ViewsService, [], false),
+    [IViewDescriptorService.toString()]: new SyncDescriptor(ViewDescriptorService, [], true),
+    [IActivityService.toString()]: new SyncDescriptor(ActivityService, [], true),
+    [IPaneCompositePartService.toString()]: new SyncDescriptor(PaneCompositePartService, [], true),
+    [IHoverService.toString()]: new SyncDescriptor(HoverService, [], true),
+
+    [ICodeEditorService.toString()]: new SyncDescriptor(CodeEditorService, [], true),
+    [ITextEditorService.toString()]: new SyncDescriptor(TextEditorService, [], false),
+    [IEditorResolverService.toString()]: new SyncDescriptor(EditorResolverService, [], false),
+    [IBreadcrumbsService.toString()]: new SyncDescriptor(BreadcrumbsService, [], true),
+    [IContextViewService.toString()]: new SyncDescriptor(ContextViewService, [], true),
+    [IUntitledTextEditorService.toString()]: new SyncDescriptor(UntitledTextEditorService, [], true),
+    [IHistoryService.toString()]: new SyncDescriptor(HistoryService, [], false),
+    [ICustomEditorService.toString()]: new SyncDescriptor(CustomEditorService, [], true),
+    [IWebviewService.toString()]: new SyncDescriptor(WebviewService, [], true),
+    [IWebviewViewService.toString()]: new SyncDescriptor(WebviewViewService, [], true),
+    [IWebviewWorkbenchService.toString()]: new SyncDescriptor(WebviewEditorService, [], true),
+    [IProgressService.toString()]: new SyncDescriptor(ProgressService, [], true),
+    [IAuxiliaryWindowService.toString()]: new SyncDescriptor(BrowserAuxiliaryWindowService, [], true),
+    [IEditorPaneService.toString()]: new SyncDescriptor(EditorPaneService, [], true),
+    [ICustomEditorLabelService.toString()]: new SyncDescriptor(CustomEditorLabelService, [], true)
+  }
+}
+
+export default getServiceOverride
