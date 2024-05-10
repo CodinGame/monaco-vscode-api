65f352b4f4fae8f4904d19fa089cba7e521682f5
Ives van Hoorne <ives@codesandbox.io>
2024-05-10 12:27:10 +0200

    fix: add missing accessibleView service

--- src/service-override/accessibility.ts
+++ src/service-override/accessibility.ts
@@ -5,6 +5,10 @@ import { AccessibilitySignalService } from 'vs/platform/accessibilitySignal/brow
 import audioAssets from 'vs/platform/accessibilitySignal/browser/media/*.mp3'
 import { IAccessibleViewService } from 'vs/workbench/contrib/accessibility/browser/accessibleView.service'
 import { IAccessibilitySignalService } from 'vs/platform/accessibilitySignal/browser/accessibilitySignalService.service'
+import { IAccessibleViewInformationService } from 'vs/workbench/services/accessibility/common/accessibleViewInformationService.service'
+import { AccessibleViewInformationService } from 'vs/workbench/services/accessibility/common/accessibleViewInformationService'
+import { IAccessibilityService } from 'vs/platform/accessibility/common/accessibility.service'
+import { AccessibilityService } from 'vs/platform/accessibility/browser/accessibilityService'
 import { registerAssets } from '../assets'
 import 'vs/workbench/contrib/accessibility/browser/accessibility.contribution'
 import 'vs/workbench/contrib/codeEditor/browser/accessibility/accessibility'
@@ -15,6 +19,8 @@ registerAssets(audioAssets)
 export default function getServiceOverride (): IEditorOverrideServices {
   return {
     [IAccessibleViewService.toString()]: new SyncDescriptor(AccessibleViewService, [], true),
-    [IAccessibilitySignalService.toString()]: new SyncDescriptor(AccessibilitySignalService, [], true)
+    [IAccessibilitySignalService.toString()]: new SyncDescriptor(AccessibilitySignalService, [], true),
+    [IAccessibilityService.toString()]: new SyncDescriptor(AccessibilityService, [], true),
+    [IAccessibleViewInformationService.toString()]: new SyncDescriptor(AccessibleViewInformationService, [], true)
   }
 }
