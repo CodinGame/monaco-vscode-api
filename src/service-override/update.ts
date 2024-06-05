0949da55e763f030befcd5856d5c43c925f575e5
Loïc Mangeonjean <loic@coderpad.io>
2024-06-05 18:40:15 +0200

    fix: add missing update service override

--- src/service-override/update.ts
+++ src/service-override/update.ts
@@ -1,7 +1,11 @@
 import { IEditorOverrideServices } from 'vs/editor/standalone/browser/standaloneServices'
+import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors'
+import { IUpdateService } from 'vs/platform/update/common/update.service'
+import { BrowserUpdateService } from 'vs/workbench/services/update/browser/updateService'
 import 'vs/workbench/contrib/update/browser/update.contribution'
 
 export default function getServiceOverride (): IEditorOverrideServices {
   return {
+    [IUpdateService.toString()]: new SyncDescriptor(BrowserUpdateService, [], true)
   }
 }
