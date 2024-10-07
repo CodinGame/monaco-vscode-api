b3ac17a12dd1cfd492a996d88aa79d92277b022b
Loïc Mangeonjean <loic@coderpad.io>
2024-10-07 16:04:09 +0200

    feat(demo): install and demo treesitter

--- demo/src/setup.common.ts
+++ demo/src/setup.common.ts
@@ -59,6 +59,7 @@ import getSurveyServiceOverride from '@codingame/monaco-vscode-survey-service-ov
 import getUpdateServiceOverride from '@codingame/monaco-vscode-update-service-override'
 import getExplorerServiceOverride from '@codingame/monaco-vscode-explorer-service-override'
 import getLocalizationServiceOverride from '@codingame/monaco-vscode-localization-service-override'
+import getTreeSitterServiceOverride from '@codingame/monaco-vscode-treesitter-service-override'
 import { EnvironmentOverride } from 'vscode/workbench'
 import { Worker } from './tools/crossOriginWorker'
 import defaultKeybindings from './user/keybindings.json?raw'
@@ -302,6 +303,7 @@ export const commonServices: IEditorOverrideServices = {
   ...getConfigurationServiceOverride(),
   ...getKeybindingsServiceOverride(),
   ...getTextmateServiceOverride(),
+  ...getTreeSitterServiceOverride(),
   ...getThemeServiceOverride(),
   ...getLanguagesServiceOverride(),
   ...getDebugServiceOverride(),
