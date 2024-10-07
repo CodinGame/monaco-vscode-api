753cd022523ed73ef6aacd9c29e3d6257d9f82b7
Loïc Mangeonjean <loic@coderpad.io>
2024-10-07 15:59:48 +0200

    feat: support new treesitter syntax highlighting

--- rollup/rollup.config.ts
+++ rollup/rollup.config.ts
@@ -581,7 +581,14 @@ export default (args: Record<string, string>): rollup.RollupOptions[] => {
       }),
       {
         name: 'dynamic-import-polyfill',
-        renderDynamicImport (): { left: string, right: string } {
+        renderDynamicImport ({ targetModuleId }): { left: string, right: string } {
+          // Hack for @vscode/tree-sitter-wasm that doesn't export its parser correctly (as default instead of a named export, in commonjs)
+          if (targetModuleId === '@vscode/tree-sitter-wasm') {
+            return {
+              left: 'import(',
+              right: ').then(module => ({ Parser: module.default ?? module }))'
+            }
+          }
           // dynamic imports of vscode-oniguruma and vscode-textmate aren't working without it on vite
           return {
             left: 'import(',
