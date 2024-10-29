e94eb7129d64b65baed6c4329b69f968214da900
Loïc Mangeonjean <loic@coderpad.io>
2024-10-29 12:10:06 +0100

    fix: use new shorted syntax for workers

--- src/workers/notebook.worker.ts
+++ src/workers/notebook.worker.ts
@@ -1,10 +1,1 @@
-import { create } from 'vs/workbench/contrib/notebook/common/services/notebookSimpleWorker'
-import { SimpleWorkerServer } from 'vs/base/common/worker/simpleWorker'
-
-const simpleWorker = new SimpleWorkerServer((msg) => {
-  globalThis.postMessage(msg)
-}, create)
-
-globalThis.onmessage = (e: MessageEvent) => {
-  simpleWorker.onmessage(e.data)
-}
+import 'vs/workbench/contrib/notebook/common/services/notebookSimpleWorker.esm.js'
--- src/workers/outputLinkComputer.worker.ts
+++ src/workers/outputLinkComputer.worker.ts
@@ -1,10 +1,1 @@
-import { SimpleWorkerServer } from 'vs/base/common/worker/simpleWorker'
-import { create } from 'vs/workbench/contrib/output/common/outputLinkComputer'
-
-const simpleWorker = new SimpleWorkerServer((msg) => {
-  globalThis.postMessage(msg)
-}, create)
-
-globalThis.onmessage = (e: MessageEvent) => {
-  simpleWorker.onmessage(e.data)
-}
+import 'vs/workbench/contrib/output/common/outputLinkComputer.esm.js'
