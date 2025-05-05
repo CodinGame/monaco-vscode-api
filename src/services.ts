39a724d2f7bf71a73b1ba2f41af9f376215b5237
Loïc Mangeonjean <loic@coderpad.io>
2025-05-05 12:24:12 +0200

    fix: fix product config override
    so that the quality is not undefined, so that it doesn't  try to use the `experimentalEditContextEnabled` option

--- src/services.ts
+++ src/services.ts
@@ -9,7 +9,6 @@ import {
   type IEditorOverrideServices,
   StandaloneServices
 } from 'vs/editor/standalone/browser/standaloneServices'
-import { mixin } from 'vs/base/common/objects'
 import {
   type GetLeadingNonServiceArgs,
   IInstantiationService,
@@ -78,23 +77,21 @@ export async function initialize(
   initializeWorkbench(container, configuration, env)
 
   const instantiationService = StandaloneServices.initialize({
-    [IProductService.toString()]: mixin(
-      <Partial<IProductConfiguration>>{
-        version: VSCODE_VERSION,
-        quality: 'stable',
-        commit: VSCODE_COMMIT,
-        nameShort: 'Code - OSS',
-        nameLong: 'Code - OSS',
-        applicationName: 'code-oss',
-        dataFolderName: '.vscode-oss',
-        urlProtocol: 'code-oss',
-        reportIssueUrl: 'https://github.com/microsoft/vscode/issues/new',
-        licenseName: 'MIT',
-        licenseUrl: 'https://github.com/microsoft/vscode/blob/main/LICENSE.txt',
-        serverApplicationName: 'code-server-oss'
-      },
-      configuration.productConfiguration ?? {}
-    ),
+    [IProductService.toString()]: <Partial<IProductConfiguration>>{
+      version: VSCODE_VERSION,
+      quality: 'stable',
+      commit: VSCODE_COMMIT,
+      nameShort: 'Code - OSS',
+      nameLong: 'Code - OSS',
+      applicationName: 'code-oss',
+      dataFolderName: '.vscode-oss',
+      urlProtocol: 'code-oss',
+      reportIssueUrl: 'https://github.com/microsoft/vscode/issues/new',
+      licenseName: 'MIT',
+      licenseUrl: 'https://github.com/microsoft/vscode/blob/main/LICENSE.txt',
+      serverApplicationName: 'code-server-oss',
+      ...(configuration.productConfiguration ?? {})
+    },
     ...getLayoutServiceOverride(), // Always override layout service to break cyclic dependency with ICodeEditorService
     ...getEnvironmentServiceOverride(),
     ...getExtensionsServiceOverride(),
