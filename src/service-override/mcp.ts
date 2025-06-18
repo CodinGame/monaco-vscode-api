2070b66a403fe206cbe6760e5b041cf990734309
Onora Hubleur <onora.hubleur@coderpad.io>
2025-06-18 16:52:35 +0200

    feat: update services for vscode 1.101.0

--- src/service-override/mcp.ts
+++ src/service-override/mcp.ts
@@ -1,17 +1,50 @@
 import { type IEditorOverrideServices } from 'vs/editor/standalone/browser/standaloneServices'
 import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors'
 import { IMcpConfigPathsService } from 'vs/workbench/contrib/mcp/common/mcpConfigPathsService.service'
-import { IMcpService } from 'vs/workbench/contrib/mcp/common/mcpTypes.service'
+import {
+  IMcpSamplingService,
+  IMcpService,
+  IMcpWorkbenchService
+} from 'vs/workbench/contrib/mcp/common/mcpTypes.service'
 import { IMcpRegistry } from 'vs/workbench/contrib/mcp/common/mcpRegistryTypes.service'
 import { McpConfigPathsService } from 'vs/workbench/contrib/mcp/common/mcpConfigPathsService'
 import { McpService } from 'vs/workbench/contrib/mcp/common/mcpService'
 import { McpRegistry } from 'vs/workbench/contrib/mcp/common/mcpRegistry'
 import 'vs/workbench/contrib/mcp/browser/mcp.contribution'
+import { IAuthenticationMcpService } from 'vs/workbench/services/authentication/browser/authenticationMcpService.service'
+import { AuthenticationMcpService } from 'vs/workbench/services/authentication/browser/authenticationMcpService'
+import { IAuthenticationMcpAccessService } from 'vs/workbench/services/authentication/browser/authenticationMcpAccessService.service'
+import { AuthenticationMcpAccessService } from 'vs/workbench/services/authentication/browser/authenticationMcpAccessService'
+import { IAuthenticationMcpUsageService } from 'vs/workbench/services/authentication/browser/authenticationMcpUsageService.service'
+import { AuthenticationMcpUsageService } from 'vs/workbench/services/authentication/browser/authenticationMcpUsageService'
+import { McpWorkbenchService } from 'vs/workbench/contrib/mcp/browser/mcpWorkbenchService'
+import {
+  IMcpGalleryService,
+  IMcpManagementService
+} from 'vs/platform/mcp/common/mcpManagement.service'
+import { McpGalleryService } from 'vs/platform/mcp/common/mcpGalleryService'
+import { McpManagementService } from 'vs/platform/mcp/common/mcpManagementService'
+import { McpSamplingService } from 'vs/workbench/contrib/mcp/common/mcpSamplingService'
 
 export default function getServiceOverride(): IEditorOverrideServices {
   return {
     [IMcpConfigPathsService.toString()]: new SyncDescriptor(McpConfigPathsService, [], true),
     [IMcpService.toString()]: new SyncDescriptor(McpService, [], true),
-    [IMcpRegistry.toString()]: new SyncDescriptor(McpRegistry, [], true)
+    [IMcpRegistry.toString()]: new SyncDescriptor(McpRegistry, [], true),
+    [IAuthenticationMcpService.toString()]: new SyncDescriptor(AuthenticationMcpService, [], true),
+    [IAuthenticationMcpAccessService.toString()]: new SyncDescriptor(
+      AuthenticationMcpAccessService,
+      [],
+      true
+    ),
+    [IAuthenticationMcpUsageService.toString()]: new SyncDescriptor(
+      AuthenticationMcpUsageService,
+      [],
+      true
+    ),
+    [IMcpWorkbenchService.toString()]: new SyncDescriptor(McpWorkbenchService, [], true),
+    [IMcpGalleryService.toString()]: new SyncDescriptor(McpGalleryService, [], true),
+    [IMcpManagementService.toString()]: new SyncDescriptor(McpManagementService, [], true),
+    [IMcpSamplingService.toString()]: new SyncDescriptor(McpSamplingService, [], true)
   }
 }
--- src/service-override/welcome.ts
+++ src/service-override/welcome.ts
@@ -1,7 +1,16 @@
 import type { IEditorOverrideServices } from 'vs/editor/standalone/browser/standaloneServices'
 import 'vs/workbench/contrib/welcomeViews/common/viewsWelcome.contribution'
 import 'vs/workbench/contrib/welcomeViews/common/newFile.contribution'
+import { IGettingStartedExperimentService } from 'vs/workbench/contrib/welcomeGettingStarted/browser/gettingStartedExpService.service'
+import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors'
+import { GettingStartedExperimentService } from 'vs/workbench/contrib/welcomeGettingStarted/browser/gettingStartedExpService'
 
 export default function getServiceOverride(): IEditorOverrideServices {
-  return {}
+  return {
+    [IGettingStartedExperimentService.toString()]: new SyncDescriptor(
+      GettingStartedExperimentService,
+      [],
+      true
+    )
+  }
 }
