0b2808b6fdd499abe57c3429f31cdb7d5d3f5cdb
Loïc Mangeonjean <loic@coderpad.io>
2025-08-11 15:44:45 +0200

    chore: adapt to worker api change

--- src/workers/editor.worker.ts
+++ src/workers/editor.worker.ts
@@ -8,27 +8,27 @@ import type { IWorkerContext } from 'vs/editor/common/services/editorWebWorker'
 
 type CreateFunction<D, R extends object = object> = (ctx: IWorkerContext<object>, data: D) => R
 export function initialize<D, R extends object>(createFn: CreateFunction<D, R>) {
-  let requestHandler: R | undefined
-  const foreignModule = new Proxy(
-    {},
-    {
-      get(_target, propKey: PropertyKey) {
-        if (propKey === '$initialize') {
-          return async (data: D) => {
-            if (!requestHandler) {
-              requestHandler = createFn(context, data)
+  start((context) => {
+    let requestHandler: R | undefined
+    return new Proxy(
+      {},
+      {
+        get(_target, propKey: PropertyKey) {
+          if (propKey === '$initialize') {
+            return async (data: D) => {
+              if (!requestHandler) {
+                requestHandler = createFn(context, data)
+              }
             }
           }
-        }
-        const value = requestHandler?.[propKey as keyof R]
+          const value = requestHandler?.[propKey as keyof R]
 
-        if (typeof value === 'function') {
-          return value.bind(requestHandler);
+          if (typeof value === 'function') {
+            return value.bind(requestHandler)
+          }
+          return value
         }
-        return value
       }
-    }
-  )
-
-  const context = start(foreignModule)
+    )
+  })
 }
