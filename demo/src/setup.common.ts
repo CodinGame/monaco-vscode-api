e760d26dbdf17960a5bb78581aedb04cc4386d2f
Loïc Mangeonjean <loic@coderpad.io>
2025-08-11 15:45:59 +0200

    lib(demo): update local dependencies
    and add new service override

--- demo/src/setup.common.ts
+++ demo/src/setup.common.ts
@@ -81,6 +81,7 @@ import getTelemetryServiceOverride from '@codingame/monaco-vscode-telemetry-serv
 import getMcpServiceOverride from '@codingame/monaco-vscode-mcp-service-override'
 import getProcessControllerServiceOverride from '@codingame/monaco-vscode-process-explorer-service-override'
 import getExperimentationServiceOverride from '@codingame/monaco-vscode-experimentation-service-override'
+import getImageResizeServiceOverride from '@codingame/monaco-vscode-image-resize-service-override'
 import { EnvironmentOverride } from '@codingame/monaco-vscode-api/workbench'
 import { Worker } from './tools/crossOriginWorker'
 import defaultKeybindings from './user/keybindings.json?raw'
@@ -547,5 +548,6 @@ export const commonServices: IEditorOverrideServices = {
   ...getTelemetryServiceOverride(),
   ...getMcpServiceOverride(),
   ...getProcessControllerServiceOverride(),
-  ...getExperimentationServiceOverride()
+  ...getExperimentationServiceOverride(),
+  ...getImageResizeServiceOverride()
 }
