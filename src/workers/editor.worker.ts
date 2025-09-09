d3fcbe903edf7151d4ca67467465a4fbb305747c
Loïc Mangeonjean <loic@coderpad.io>
2025-09-09 12:34:05 +0200

    cleanup: remove backward compatibility code

--- src/workers/editor.worker.ts
+++ src/workers/editor.worker.ts
@@ -1,34 +1,27 @@
-import 'vs/editor/common/services/editorWebWorkerMain.js'
-import { start } from 'vs/editor/editor.worker.start'
 import type { IWorkerContext } from 'vs/editor/common/services/editorWebWorker'
-
-/**
- * Make the `initialize` method backward compatible, until the monaco standalone workers are updated
- */
+import 'vs/editor/common/services/editorWebWorkerMain.js'
+import * as worker from 'vs/editor/editor.worker.start'
 
 type CreateFunction<D, R extends object = object> = (ctx: IWorkerContext<object>, data: D) => R
-export function initialize<D, R extends object>(createFn: CreateFunction<D, R>) {
-  start((context) => {
-    let requestHandler: R | undefined
-    return new Proxy(
-      {},
-      {
-        get(_target, propKey: PropertyKey) {
-          if (propKey === '$initialize') {
-            return async (data: D) => {
-              if (!requestHandler) {
-                requestHandler = createFn(context, data)
-              }
-            }
-          }
-          const value = requestHandler?.[propKey as keyof R]
 
-          if (typeof value === 'function') {
-            return value.bind(requestHandler)
-          }
-          return value
-        }
-      }
-    )
-  })
+let initialized = false
+function isWorkerInitialized() {
+  return initialized
 }
+function initialize<D, R extends object>(createFn: CreateFunction<D, R>) {
+  initialized = true
+  self.onmessage = (m) => {
+    worker.start((ctx) => {
+      return createFn(ctx, m.data)
+    })
+  }
+}
+
+self.onmessage = () => {
+  if (!isWorkerInitialized()) {
+    worker.start(() => {
+      return {}
+    })
+  }
+}
+export { initialize }
