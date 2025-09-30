daa4de202da3f074b9820a927d69c2e1160754ba
Loïc Mangeonjean <loic@coderpad.io>
2025-09-30 19:03:32 +0200

    fix: use existing contribution instead of doing by hands

--- src/service-override/textmate.ts
+++ src/service-override/textmate.ts
@@ -1,26 +1,11 @@
-import {
-  type IEditorOverrideServices,
-  StandaloneServices
-} from 'vs/editor/standalone/browser/standaloneServices'
+import { type IEditorOverrideServices } from 'vs/editor/standalone/browser/standaloneServices'
 import { ITextMateTokenizationService } from 'vs/workbench/services/textMate/browser/textMateTokenizationFeature.service'
 import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors'
 import { TextMateTokenizationFeature } from 'vs/workbench/services/textMate/browser/textMateTokenizationFeatureImpl'
-import { LifecyclePhase } from 'vs/workbench/services/lifecycle/common/lifecycle'
-import { ILifecycleService } from 'vs/workbench/services/lifecycle/common/lifecycle.service'
 import getFileServiceOverride from './files'
-import { registerServiceInitializeParticipant } from '../lifecycle'
 import 'vs/workbench/services/themes/common/tokenClassificationExtensionPoint'
 import 'vs/workbench/contrib/codeEditor/browser/inspectEditorTokens/inspectEditorTokens'
-
-registerServiceInitializeParticipant(async (accessor) => {
-  void accessor
-    .get(ILifecycleService)
-    .when(LifecyclePhase.Ready)
-    .then(() => {
-      // Force load the service
-      StandaloneServices.get(ITextMateTokenizationService)
-    })
-})
+import 'vs/workbench/services/textMate/browser/textMateTokenizationFeature.contribution'
 
 export default function getServiceOverride(): IEditorOverrideServices {
   return {
