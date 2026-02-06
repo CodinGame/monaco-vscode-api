77c6c41cc6c4681ef0ef6d413d72c72427f173e6
Loïc Mangeonjean <loic@coderpad.io>
2026-02-06 15:23:10 +0100

    fix(css-import-plugin): generate asset sooner, and properly replace them in output

--- rollup/plugins/css-import-plugin.ts
+++ rollup/plugins/css-import-plugin.ts
@@ -17,7 +17,6 @@ export default ({
   include = ['**/*.css'],
   exclude = []
 }: Options = {}): Plugin => {
-  const styles: Record<string, string> = {}
   const filter = createFilter(include, exclude)
 
   return {
@@ -45,58 +44,76 @@ export default ({
     },
 
     /**
-     * Transform the css into a module containing it
+     * Transform the css into a module containing it, also resolve assets and emit rollup assets for them
      */
-    transform(code, id) {
+    async transform(code, id) {
       if (!filter(id)) return
 
-      styles[id] = code
+      const replacements: Record<string, string> = {}
+
+      await postcss([
+        postcssUrl({
+          url: async (asset) => {
+            let fileName: string | undefined
+            if (preserveAssetsRoot != null) {
+              fileName = path
+                .relative(path.resolve(process.cwd(), preserveAssetsRoot), asset.absolutePath!)
+                .replace(/^(\.\.\/)+/g, '')
+            }
+
+            const resolved = await this.resolve(asset.url, id)
+            if (resolved != null && !resolved.external) {
+              const assetId = this.emitFile({
+                type: 'asset',
+                fileName,
+                source: await fs.promises.readFile(resolved.id),
+                needsCodeReference: false
+              })
+
+              replacements[asset.url] = assetId
+            }
+
+            return asset.url
+          }
+        })
+      ]).process(code, { from: id })
 
       return {
-        code: `renderCSS(${JSON.stringify(code)}); export default 'fakeExport'`
+        code: `renderCSS(${JSON.stringify(code)}, {${Object.entries(replacements)
+          .map(([url, assetId]) => `"${url}": import.meta.ROLLUP_FILE_URL_${assetId}`)
+          .join(', ')}}); export default 'fakeExport'`
       }
     },
 
     /**
-     * transform back the chunk into a css file, and emit referenced asset files
+     * transform back the chunk into a css file, replacing the urls with the emitted ones
      */
     async renderChunk(code, chunk) {
       if (chunk.fileName.endsWith('.css')) {
         const parsed = acorn.parse(code, { ecmaVersion: 2022, sourceType: 'module' })
-        const css = (
-          ((parsed.body[0] as acorn.ExpressionStatement).expression as acorn.CallExpression)
-            .arguments[0] as acorn.Literal
-        ).value as string
 
-        await postcss([
-          postcssUrl({
-            url: async (asset, dir) => {
-              let fileName: string | undefined
-              if (preserveAssetsRoot != null) {
-                fileName = path
-                  .relative(path.resolve(process.cwd(), preserveAssetsRoot), asset.absolutePath!)
-                  .replace(/^(\.\.\/)+/g, '')
-              }
-
-              const resolved = await this.resolve(
-                asset.url,
-                dir.file != null ? path.join(dir.file, 'unknownFile.css') : undefined
-              )
-              if (resolved != null && !resolved.external) {
-                this.emitFile({
-                  type: 'asset',
-                  fileName,
-                  source: await fs.promises.readFile(resolved.id),
-                  needsCodeReference: false
-                })
-              }
-
-              return asset.url
+        const renderCssArguments = (
+          (parsed.body[0] as acorn.ExpressionStatement).expression as acorn.CallExpression
+        ).arguments
+        const css = (renderCssArguments[0] as acorn.Literal).value as string
+        const replacements = (renderCssArguments[1] as acorn.ObjectExpression).properties.reduce(
+          (acc, prop) => {
+            if (
+              prop.type === 'Property' &&
+              prop.key.type === 'Literal' &&
+              prop.value.type === 'Literal'
+            ) {
+              acc[prop.key.value as string] = prop.value.value as string
             }
-          })
-        ]).process(css, { from: chunk.fileName })
+            return acc
+          },
+          {} as Record<string, string>
+        )
 
-        return css
+        return Object.entries(replacements).reduce(
+          (acc, [url, replacedUrl]) => acc.replaceAll(url, replacedUrl),
+          css
+        )
       }
 
       return null
