5b479571498f99da40491e9abc87bd7f4d1792c1
Loïc Mangeonjean <loic@coderpad.io>
2026-04-28 10:14:18 +0200

    lib!: update to vscode 1.117

--- src/service-override/welcome.ts
+++ src/service-override/welcome.ts
@@ -1,7 +1,13 @@
 import type { IEditorOverrideServices } from 'vs/editor/standalone/browser/standaloneServices'
+import { IOnboardingService } from 'vs/workbench/contrib/welcomeOnboarding/common/onboardingService.service'
+import { OnboardingVariationA } from 'vs/workbench/contrib/welcomeOnboarding/browser/onboardingVariationA'
+import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors'
 import 'vs/workbench/contrib/welcomeViews/common/viewsWelcome.contribution'
 import 'vs/workbench/contrib/welcomeViews/common/newFile.contribution'
+import 'vs/workbench/contrib/welcomeOnboarding/browser/welcomeOnboarding.contribution'
 
 export default function getServiceOverride(): IEditorOverrideServices {
-  return {}
+  return {
+    [IOnboardingService.toString()]: new SyncDescriptor(OnboardingVariationA, [], true)
+  }
 }
